842c908be91bd5acea5aef99c0a70973855286d8
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-10-28 09:28:27 +0100

    Add title and labels to a ggplot

---
+++ Google_data_analytics_course_7/c7w4_ggplot_annotations.R
@@ -0,0 +1,16 @@
+# c7w4 ggplot annotations
+library(tidyverse)
+library(palmerpenguins)
+data(penguins)
+
+plot <- ggplot(data=penguins)+
+  geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g, color=species))+
+  labs(title="Palmer penguins: body masss vs flipper lenght", 
+       subtitle="Sample of three penguin species",
+       caption="Data collected by Dr. Kristen Gorman")+
+  annotate("text", x=220, y =3500, label="the Gentoos are the largest", 
+           color="blue", fontface="bold", size=4.5, angle=25)
+
+plot + annotate("text", x=190, y=5000,
+               label="Adelie and Chinstrap are equally sized", color="black",
+             fontface="bold")
