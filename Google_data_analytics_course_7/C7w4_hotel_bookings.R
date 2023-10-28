e2392b06e443f1f357b66d2e95c903a9c6d9fdf4
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-10-28 09:53:26 +0100

    Plot hotel bookings vs market segment

---
+++ Google_data_analytics_course_7/C7w4_hotel_bookings.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+hotel_bookings <- read.csv("hotel_bookings.csv")
+str(hotel_bookings)
+
+# plot booking counts for each market segment and hotel type
+plot <- ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  labs(title="Comparison of hotel bookings by market segment and hotel type")
+mindate <- min(hotel_bookings$arrival_date_year)
+maxdate <- max(hotel_bookings$arrival_date_year)
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       caption=paste0("Data from: ", mindate, " to ", maxdate),
+       x="Market Segment",
+       y="Number of Bookings")
+
+ggsave("hotelbookings_marketsegment.png")
